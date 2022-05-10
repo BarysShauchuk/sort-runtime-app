a69ab22eded788f7e6ac85cc5655f64fa3e81ca7
BarysShauchuk <borshevchukn@gmail.com>
2022-05-10 17:16:19 +0300

    fix: change window's name and icon

--- sort-runtime-app/Form1.Designer.cs
+++ sort-runtime-app/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
             this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -188,9 +189,10 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.cartesianChart1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Sorting algorithms\' runtime";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
